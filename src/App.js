ed2e4b2b26e3cafc39356215c33e624d954cc008
3IN4R <eaaeibl@students.eaaa.dk>
2023-02-02 19:33:16 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/home';
+import Info from './pages/info';
+import Landing from './pages/landing';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ function App() {
+   
+
+    return(
+      <div className='App'>
+        <Routes>
+     <Route path='/home' element={<Home/>}/>
+     <Route path='/info' element={<Info/>}/>
+    <Route path='/' element={<Landing/>}/>
+     </Routes>
+        </div>
+      
+    )
 }
 
 export default App;
