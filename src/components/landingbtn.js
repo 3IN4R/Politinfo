511c9b8c19b653390a3d119c8abc895a2a842003
Einar <89975291+3IN4R@users.noreply.github.com>
2023-02-02 18:29:10 +0100

    Delete src directory

--- src/components/landingbtn.js
+++
@@ -1,9 +0,0 @@
-
-export default function LandingBtn(props){
-    return(
-    
-            <button onClick={props.link} className='trash'>
-            <h3>{props.account}</h3></button>
-        
-    )
-}
